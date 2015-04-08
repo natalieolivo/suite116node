ba9c5b929539ce06d28c2c520cd04a725e65ba7d
nolivo1 <nolivo1@gmail.com>
2015-04-07 20:15:16 -0400

    R1.0 - Set handlebars as view engine
    * Add Handlebar template files for homepage partials

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 var app = require('express')()
 	, server = require('./server')(app)
-	, router = require('./router')(app);
+	, router = require('./router')(app)
+	, path = require('path')
+	, hbs = require('hbs')
+	, fs = require('fs')
+	, _ = require('underscore');
+
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'hbs');
+
+hbs.registerPartials(__dirname + '/views/partials');
